6827bcdc98675a241ceb7bb05271aee32d381f29
Vlad <vlad220994@gmail.com>
2019-07-17 13:45:12 +0300

    transform data from API

--- src/services/SwapiService.js
+++ src/services/SwapiService.js
@@ -13,42 +13,75 @@ export default class SwapiService {
     return await res.json();
   }
 
-  async getAllPeople() {
-
+  async getAllPerson() {
     const res = await this.getResource('/people/');
-
-    return  res.results;
+    return  res.results.map(this._transormPerson);
   }
 
-  getPeople(id) {
-    return this.getResource(`/people/${id}/`)
+  async getPerson(id) {
+    const people = this.getResource(`/people/${id}/`);
+    return this._transormPerson(people);
   }
 
   async getAllPlanets() {
-    
     const res = await this.getResource(`/planets/`);
-
-    return res.results;
+    return res.results.map(this._transormPlanet);
   }
 
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}/`);
+  async getPlanet(id) {
+    const planet = await this.getResource(`/planets/${id}/`);
+    return this._transormPlanet(planet);
   }
 
   async getAllStarships() {
-    
     const res = await this.getResource(`/starships/`);
-
-    return res.results;
+    return res.results.map(this._transormStarship);
   }
 
-  getStarship(id) {
-    return this.getResource(`/starships/${id}/`);
+  async getStarship(id) {
+    const starship = this.getResource(`/starships/${id}/`);
+    return this._transormStarship(starship);
+  }
+
+  _extractId(item) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    return item.url.match(idRegExp)[1];
+  }
+
+  _transormPlanet(planet) {
+
+    return {
+      id: this._extractId(planet),
+      name: planet.name,
+      population: planet.population,
+      rotationPeriod: planet.rotation_period,
+      diameter: planet.diameter
+    }
+  }
+
+  _transormStarship(starship) {
+
+    return {
+      id: this._extractId(starship),
+      name: starship.name,
+      model: starship.model,
+      manufacturer: starship.manufacturer,
+      costInCredits: starship.costInCredits,
+      length: starship.length,
+      crew: starship.crew,
+      passengers: starship.passengers,
+      cargoCapacity: starship.cargoCapacity
+    }
+  }
+
+  _transormPerson(person) {
+
+    return {
+      id: this._extractId(person),
+      name: person.name,
+      gender: person.gender,
+      birthYear: person.birthYear,
+      eyeColor: person.eyeColor
+    }
   }
 }
-
-const hero = new SwapiService();
-
-hero.getPeople(2).then((element) =>
-    console.log(element)
-);
